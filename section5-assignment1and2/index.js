1d9430f0f5cd0c2f0866e8533202f9c3a4bfcae2
dsonn <sbdsonn@gmail.com>
2019-04-22 18:15:13 -0400

    fixed multiple load error

--- section5-assignment1and2/index.js
+++ section5-assignment1and2/index.js
@@ -1,58 +1,43 @@
-'use strict';
+"use-strict";
 
 let STORE = {
-  picArray: [],
-  appIsReady: false,
+  picArray: []
 };
 
-// Take breed input
-// Is breed valid? Get list of all breeds at app start?
-//
+function watchForm() {
+  $("form").submit(event => {
+    event.preventDefault();
+    if ($(".js-num-pic").val() > 50) {
+      return alert("please choose between 1-50");
+    }
+    getDogImage($(".js-num-pic").val());
+    $(".js-num-pic").val("");
+  });
+}
 
 function getDogImage(numPic) {
   fetch(`https://dog.ceo/api/breeds/image/random/${numPic}`)
     .then(response => response.json())
-    // .then(responseJson => 
-    //   displayResults(responseJson))
-    .then(responseJson => 
-      makeArray(responseJson))
+    .then(responseJson => (STORE.picArray = responseJson.message))
     .then(displayResults)
-    .catch(error => alert('Something went wrong. Try again later.'));
+    .catch(error => alert("Something went wrong. Try again later."));
+}
+
+function displayResults() {
+  $(".results-img").html(htmlTemplate());
+  $(".results").removeClass("hidden");
 }
 
 function htmlTemplate() {
   let temp = [];
   for (let i = 0; i < STORE.picArray.length; i++) {
-    temp.push(`<img src="${STORE.picArray[i]}" class="results-img">`)
+    temp.push(`<img src="${STORE.picArray[i]}" class="results-img">`);
   }
-  return temp.join('');
-}
-
-function displayResults() {
-  //console.log(responseJson);
-  //replace the existing image with the new one
-  $('.results-img').replaceWith(htmlTemplate());
-  //display the results section
-  $('.results').removeClass('hidden');
-}
-
-function makeArray(responseJson) {// convert response to array
-  STORE.picArray = responseJson.message;
-  console.log(STORE.picArray);
-}
-
-function watchForm() {
-  $('form').submit(event => {
-    event.preventDefault();
-    if($('.js-num-pic').val() > 50) {
-      alert('please choose between 1-50');
-      return;
-    }
-    getDogImage($('.js-num-pic').val());
-  });
+  console.log(temp);
+  return temp.join("");
 }
 
 $(function() {
-  console.log('App loaded! Waiting for submit!');
+  console.log("App loaded! Waiting for submit!");
   watchForm();
 });
